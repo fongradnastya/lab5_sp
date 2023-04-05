6305174ce9184fe6791308ac860f0e88a9760718
Anastasia Fongrad <fongradnastya@gmail.com>
2023-04-05 23:18:50 +0700

    Deleted files

--- interface.c
+++
@@ -1,7 +0,0 @@
-void writeProgramDescription()
-{
-    printf("---------------------------------------\n");
-    printf("This programm works with squate matrix\n");
-    printf("---------------------------------------\n");
-}
-
